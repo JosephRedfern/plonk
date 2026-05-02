9545d49b3dc423d964594c3d5d8ba0a5f92adc73
Joe Redfern <joseph@redfern.me>
2026-05-02 15:09:50 +0100

    Initial Commit

---
+++ pypist/pypistApp.swift
@@ -0,0 +1,17 @@
+//
+//  pypistApp.swift
+//  pypist
+//
+//  Created by Joe Redfern on 02/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct pypistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
